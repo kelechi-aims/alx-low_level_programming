ffc8ac8c11dbf1973d758b681361ac3afc7a1976
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-02-16 12:33:58 -0800

    task-4 of C Hello, world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");/*outputs the string with a new line*/
+
+	return (0); /*returning the value 0*/
+}
