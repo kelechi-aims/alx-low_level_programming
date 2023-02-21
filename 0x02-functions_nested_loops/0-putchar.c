7af691e7cd19e8c3cbf8f6589313e4ca81e4c126
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-02-21 08:39:25 -0800

    task-0 of C - Function, nested, loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - This prints _putchar, followed by a new line
+ *
+ * Return: The value 0
+ */
+
+int main(void)
+{
+	char my_word[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int i;
+
+	for (i = 0; i < 8; ++i)
+	{
+		_putchar(my_word[i]);
+	}
+	_putchar('\n');
+	return (0);
+
+}
