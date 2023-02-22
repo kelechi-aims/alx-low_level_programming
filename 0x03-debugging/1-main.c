ae7e20053028866099b7ae86ee93c89424d65364
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-02-22 14:14:44 -0800

    task-1 of C-Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,8 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-/**	i = 0;
+/**
+ *	i = 0;
  *
  *	while (i < 10)
  *	{
@@ -19,6 +20,5 @@ int main(void)
  *	}
  */
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
