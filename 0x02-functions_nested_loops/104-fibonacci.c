144eb1141d1c89c3638922a150142c7e0da7e8d8
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-02-22 11:31:15 -0800

    task-16 of C-Functions,nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,40 +8,40 @@
 
 int main(void)
 {
-	int i;
-	unsigned long fn = 0, sn = 1, fb;
-	unsigned long n1, n2, n3, n4, sum1, sum2;
+int i;
+unsigned long fn = 0, sn = 1, fb;
+unsigned long n1, n2, n3, n4, sum1, sum2;
 
-	for (i = 0; i < 80; ++i)
-	{
-		fb = fn + sn;
-		printf("%ld, ", fb);
-		fn = sn;
-		sn = fb;
-	}
-	n1 = fn / 10000000000;
-	n2 = fn % 10000000000;
-	n3 = sn / 10000000000;
-	n4 = sn % 10000000000;
-	for (i = 81; i < 99; ++i)
-	{
-		sum1 = n1 + n3;
-		sum2 = n2 + n4;
-		if ((n2 + n4) > 999999999)
-		{
-			sum1 += 1;
-			sum2 %= 10000000000;
-		}
-		printf("%ld%ld", sum1, sum2);
-		if (i != 98)
-		{
-			printf(", ");
-		}
-		n1 = n3;
-		n2 = n4;
-		n3 = sum1;
-		n4 = sum2;
-	}
-	printf("\n");
-	return (0);
+for (i = 0; i < 92; ++i)
+{
+fb = fn + sn;
+printf("%ld, ", fb);
+fn = sn;
+sn = fb;
+}
+n1 = fn / 10000000000;
+n2 = fn % 10000000000;
+n3 = sn / 10000000000;
+n4 = sn % 10000000000;
+for (i = 93; i < 99; ++i)
+{
+sum1 = n1 + n3;
+sum2 = n2 + n4;
+if ((n2 + n4) > 999999999)
+{
+sum1 += 1;
+sum2 %= 10000000000;
+}
+printf("%ld%ld", sum1, sum2);
+if (i != 98)
+{
+printf(", ");
+}
+n1 = n3;
+n2 = n4;
+n3 = sum1;
+n4 = sum2;
+}
+printf("\n");
+return (0);
 }
