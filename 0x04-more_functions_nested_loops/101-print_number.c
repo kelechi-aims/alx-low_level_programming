e43fb0b39dd55e2cc63c0f2f5d738107a77cf3f0
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-02-23 12:08:34 -0800

    task-12 of More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_number - prints and integer
+ * @n: function's parameter
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n > 0)
+	{
+		print_num(n);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_num(-1 * n);
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
+
+/**
+ * print_num - prints larger integer
+ * @q: parameter of the function
+ *
+ * Return: void
+ */
+void print_num(int q)
+{
+	int i = 1000000000;
+
+	for (; i >= 1; i /= 10)
+	{
+		if (q / i != 0)
+		{
+			_putchar((q / i) % 10 + '0');
+		}
+	}
+}
