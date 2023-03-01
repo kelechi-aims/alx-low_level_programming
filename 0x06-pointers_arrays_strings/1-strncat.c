6b612bdbc2dbd948cc7f12c1fb0293fea9a8d56f
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-01 11:28:51 -0800

    task-1 of more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: first character parameter
+ * @src: second charater parameter
+ * @n: third parameter and an integer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0, srclen = 0;
+
+	while (dest[destlen] != '\0')
+	{
+		destlen++;
+	}
+	while (srclen < n)
+	{
+		dest[destlen] = src[srclen];
+		if (src[srclen] == '\0')
+		{
+			break;
+		}
+		destlen++;
+		srclen++;
+	}
+	return (dest);
+}
