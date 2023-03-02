e81c6501e80819b4c8b411310f16f78c233bd250
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-02 08:23:27 -0800

    task-8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,11 +2,29 @@
 
 /**
  * *rot13 - encodes a string using rot13
- * @str: string parameter
+ * @s: string parameter
  *
  * Return: string
  */
-char *rot13(char *)
+char *rot13(char *s)
 {
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+			{
+				s[i] = s[i] + 13;
+			}
+			else
+			{
+				s[i] = s[i] - 13;
+			}
+			i++;
+		}
+		i++;
+	}
+	return (s);
 }
