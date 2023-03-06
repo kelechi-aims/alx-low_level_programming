11b8f9b0e49ea23eac1a85138125307f1a3e6ff5
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-06 10:56:36 -0800

    task-6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - the chessboard
+ * @: array pionter
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j <=7; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
