e3d047f3cc0e10b69514c317ee136c71b4bad7bb
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-06 04:14:20 -0800

    task-0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory
+ * @s: destination string
+ * @b: source string
+ * @n: number of bytes to occupy
+ * 
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
