28dafd800eb152faafce4ddeea507d9e28f33ae1
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-07 07:53:47 -0800

    correction

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * *infinite_add - adds two numbers
+ * @n1: string of numbers
+ * @n2: second string of numbers
+ * @r: buffer that stores the sum of the two strings
+ * @size_r: size of the buffer
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	:wq
+
+}
