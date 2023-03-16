1771fa94c41301ffccbfd170aa2df32904a75aea
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-16 13:19:51 -0700

    task-4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,24 +12,27 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new;
+	
 
-	if (new_size == 0 && ptr != NULL)
+	if (new_size > old_size)
 	{
 		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
 		new = malloc(new_size);
 	}
+
 	if (new_size == old_size)
 	{
 		return (ptr);
 	}
-	if (new_size > old_size)
+
+	if (ptr == NULL)
 	{
-	free(ptr);
-	new = malloc(new_size);
+		new = malloc(new_size);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
 	}
 	return (new);
 }
