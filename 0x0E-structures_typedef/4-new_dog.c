a60f629b26c88c1c77bf1edd2c8c7f41e31cbb93
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-20 15:49:56 -0700

    correction

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -31,13 +31,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(new_dog->name, name);
 	new_dog->owner = malloc(sizeof(len2));
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
+	strcpy(new_dog->name, name);
 	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
 	return (new_dog);
