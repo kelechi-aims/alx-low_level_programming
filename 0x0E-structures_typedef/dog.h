2292dc2cd29304d9819fc009cde18a46015ce9b4
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-20 11:57:26 -0700

    task-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+/**
+ * dog - user data type
+ * @name: first parameter
+ * @age: second parameter
+ * @owner: third parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
