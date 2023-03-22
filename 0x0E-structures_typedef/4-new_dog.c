fdcff425ec7d694a82c7db0dbddc5c19bfe78685
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-22 09:11:22 -0700

    deletions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,8 +34,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(sizeof(len2));
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog);
 		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 	strcpy(new_dog->name, name);
