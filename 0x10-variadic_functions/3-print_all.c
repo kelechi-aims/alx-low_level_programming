37ebef27001e940b80e898a41d491d2ccefcd38f
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-03-23 10:54:46 -0700

    task-3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ * @...: list of variable data types
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int j = 0;
+	char *s;
+	va_list ap;
+
+	va_start(ap, format);
+	while (format[j] != '\0')
+	{
+		switch (format[j])
+		{
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+				}
+				else
+				{
+					printf("%s", s);
+				}
+				break;
+		}
+		if ((format[j] == 'i' || format[j] == 'c' || format[j] == 'f'
+					|| format[j] == 's')  && format[(j + 1)] != '\0')
+			printf(", ");
+		j++;
+	}
+	printf("\n");
+	va_end(ap);
+}
