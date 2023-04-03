50aafb5d5fb88f409875377fe15b8ac42aef0c0f
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-04-03 00:08:30 -0700

    task-6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+	section .data
+msg:	db 'Hello, Holberton', 0Ah, 0
+
+	section .text
+	global main
+	extern printf
+
+main:
+	push rbp
+	mov rbp, rsp
+
+	lea rdi, [msg]
+	xor eax, eax
+	call printf
+
+	mov rsp,rbp
+	pop rbp
+	xor eax, eax
+	ret
