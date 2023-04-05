248db1628e38376815cbb3ba1ebf79f198400af2
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-04-05 00:55:55 -0700

    task-0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: a pointer to the head node of listint_t list
+ * @h: a pointer to the head node of the listint_t list
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
