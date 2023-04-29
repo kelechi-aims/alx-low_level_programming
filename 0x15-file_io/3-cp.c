08fbfa2c9a0d113ff1f39e0eb155d7a55904da5d
Arukwe Kelechi Denise <kelechi.denise@yahoo.com>
2023-04-29 04:15:03 -0700

    returning changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,11 +8,10 @@
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, bytes_read, bytes_written;
+	int file_from, file_to;
+	ssize_t bytes_read, bytes_written;
 	char buff[1024];
-	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	struct stat file_stat;
-
+	
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -24,26 +23,26 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
+	bytes_read = read(file_from, buff, 1024);
 	while (bytes_read == 1024)
 	{
-		bytes_read = read(file_from, buff, sizeof(buff));
-		if (bytes_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
 		bytes_written = write(file_to, buff, bytes_read);
 		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 			exit(99);
 		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	if (close(file_from) == -1)
 	{
@@ -56,4 +55,4 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 	}
 	return (0);
-
+}
