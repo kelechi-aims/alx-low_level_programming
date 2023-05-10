48e573d924745d324ab2b70151cd2732c427579b
kelechi-aims <kelechi@aims.edu.gh>
2023-05-10 03:07:33 -0700

    still correcting

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,7 +31,7 @@ void copy_file(int file_from, int file_to, char **argv)
 		bytes_written = write(file_to, buff, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
@@ -82,7 +82,7 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	copy_file(file_from, file_to, argv);
