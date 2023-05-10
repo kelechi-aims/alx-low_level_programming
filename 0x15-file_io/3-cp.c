1e8d2105700acde5ca27424b2176b4d4983b7400
kelechi-aims <kelechi@aims.edu.gh>
2023-05-10 01:13:54 -0700

    task 3 correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,12 +6,12 @@
  * @argv: argument vector
  * Return: integer
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int file_from, file_to;
 	ssize_t bytes_read, bytes_written;
 	char buff[1024];
-	
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -23,14 +23,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	bytes_read = read(file_from, buff, 1024);
-	while (bytes_read == 1024)
+	while ((bytes_read = read(file_from, buff, 1024)) > 0)
 	{
 		bytes_written = write(file_to, buff, bytes_read);
 		if (bytes_written == -1)
@@ -47,12 +46,12 @@ int main(int argc, char *argv[])
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		close(file_to);
 		exit(100);
 	}
 	if (close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
 	}
 	return (0);
 }
