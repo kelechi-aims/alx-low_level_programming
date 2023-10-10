1a4e4891cfac5fc568f27fbf49d944fd5652f7e9
kelechi-aims <kelechi@aims.edu.gh>
2023-10-10 09:05:18 +0100

    header file upload

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,4 @@
-#ifndef SEARCH_ALGOS_H_
+#ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
@@ -10,8 +10,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-/* listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-*/
 
 #endif
